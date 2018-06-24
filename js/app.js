e09148d7e825b68201debbe2629b8544d658a9ce
Johnny <johnny@offspringdigital.com>
2018-06-25 03:22:10 +0700

    Attach events, render items

--- js/app.js
+++ js/app.js
@@ -44,20 +44,54 @@
     return a;
   };
 
+  var guid = function () {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4()
+  };
+
+  var createDomLi = function (item, index, wrapperId) {
+
+    var li = document.createElement("li");
+    li.className = 'oac__item';
+    li.setAttribute('data-wrapper-id', wrapperId);
+    li.setAttribute('data-index', index);
+    var divImg = document.createElement("div");
+    divImg.className = 'oac__img';
+    divImg.setAttribute('data-index', index);
+    divImg.setAttribute('data-wrapper-id', wrapperId);
+    var divName = document.createElement("div");
+    divName.className = 'oac__name';
+    divName.setAttribute('data-index', index);
+    divName.innerHTML = item.name;
+    divName.setAttribute('data-wrapper-id', wrapperId);
+
+    li.appendChild(divImg);
+    li.appendChild(divName);
+    li.appendChild(divName);
+
+    return li;
+  };
+
   $.fn.ac = function (options, items) {
     var _ = this;
 
     _.options = {
-      idListItem: 'xoac',
       cb_after_update_logs: undefined,
       cb_error_set_items: undefined
     };
     _.options = objectExtend(this.options, options);
-    _.items = items || [];
+    _.originalItems = items || [];
     _.selectedItems = [];
     _.searchedKeywords = [];
     _.logs = [];
     _.suggestLayer = null;
+    _.timeoutKeyup = null;
+    _.superElements = [];
+    _.superElementsObj = {};
 
     _.init();
     return _;
@@ -66,8 +100,22 @@
   $.fn.init = function () {
     var _ = this;
 
+    // Init elements
+    for (var i = 0, len = _.elements.length; i < len; i++) {
+      var elmData = {
+        idWrapper: guid(),
+        idListItem: guid(),
+        dom: _.elements[i],
+        suggestLayer: null,
+        timeoutKeyup: null,
+        items: _.originalItems
+      };
 
-    // Create dom list for items & append to body
+      _.superElements.push(elmData);
+      _.superElementsObj[elmData.idWrapper] = elmData;
+    }
+
+    // Create dom list for items
     _.renderForFirstTime();
 
     _.bindEvents();
@@ -78,85 +126,121 @@
   $.fn.bindEvents = function () {
     var _ = this;
     // Bind event: onkeyup for selector
-    for (var i = 0, len = _.elements.length; i < len; i++) {
-      _.elements[i].addEventListener("focus", function () {
-        _.showSuggestLayer();
+    for (var i = 0, len = _.superElements.length; i < len; i++) {
+      var se = _.superElements[i];
+      se.dom.addEventListener("focus", function () {
+        _.showSuggestLayer(this.getAttribute('data-wrapper-id'));
       });
-      _.elements[i].addEventListener("blur", function () {
-        _.hideSuggestLayer();
+      se.dom.addEventListener("blur", function () {
+        var wrapperId = this.getAttribute('data-wrapper-id');
+        setTimeout(function () {
+          _.hideSuggestLayer(wrapperId);
+        }, 100)
+      });
+      se.dom.addEventListener("keyup", function () {
+        var wrapperId = this.getAttribute('data-wrapper-id');
+        var keyword = this.value;
+        _.timeoutKeyup = setTimeout(function () {
+          _.query(keyword, wrapperId);
+        }, 300);
+      });
+
+      // Bind event: for wrapper - event delegation
+      document.getElementById(se.idListItem).addEventListener("click", function (e) {
+        // e.target was the clicked element
+        console.log(e.target.getAttribute('data-index'));
+        if (e.target && e.target.matches("a.classA")) {
+          console.log("Anchor element clicked!");
+        }
       });
     }
-
-    // Bind event: for wrapper - event delegation
-
 
     // Bind event: arrow key for list items
   };
 
-  $.fn.getItems = function () {
+  $.fn.query = function (keyword, wrapperId) {
+    var _ = this;
+    var newItems = [];
 
-    return _.items;
+    for (var i = 0, len = _.originalItems.length; i < len; i++) {
+      if (-1 < _.originalItems[i].name.toLowerCase().indexOf(keyword)) {
+        newItems.push(_.originalItems[i])
+      }
+    }
+
+    console.log(newItems, wrapperId)
+
+    _.setItems(newItems, wrapperId);
   };
 
-  $.fn.setItems = function (data) {
+  $.fn.getItems = function () {
+
+    return this.items;
+  };
+
+  $.fn.setItems = function (items, wrapperId) {
     var _ = this;
 
     // Convert text to json object
-    try {
-      var items = JSON.parse(data);
-    } catch(e) {
-      typeof _.options.cb_error_set_items === 'function' && _.options.cb_error_set_items(e, _);
-    }
+    // try {
+    //   var items = JSON.parse(data);
+    // } catch (e) {
+    //   typeof _.options.cb_error_set_items === 'function' && _.options.cb_error_set_items(e, _);
+    // }
 
     // Validate format of items variable before updating
-    _.items = items;
+    _.superElementsObj[wrapperId].items = items;
 
     // If data is valid
     // --> clear all history
     _.clearHistory();
     // --> re-render dom
-    _.render();
+    _.render(wrapperId);
 
     return _;
   };
 
   $.fn.renderForFirstTime = function () {
     var _ = this;
-    var container = document.createElement("div");
-    container.className = 'oac box-shadow';
-    var ul = document.createElement("ul");
-    ul.className = 'oac__list';
-    ul.setAttribute('id', _.options.idListItem);
 
-    for (var i = 0, len = _.items.length; i < len; i++) {
+    for (var i = 0, len = _.superElements.length; i < len; i++) {
+      var se = _.superElements[i];
 
-      var li = document.createElement("li");
-      li.className = 'oac__item';
-      li.setAttribute('data-index', i);
-      var divImg = document.createElement("div");
-      divImg.className = 'oac__img';
-      var divName = document.createElement("div");
-      divName.className = 'oac__name';
-      divName.innerHTML = _.items[i].name;
+      var container = document.createElement("div");
+      container.className = 'oac box-shadow';
+      container.setAttribute('id', se.idWrapper);
+      var ul = document.createElement("ul");
+      ul.className = 'oac__list';
+      ul.setAttribute('id', se.idListItem);
+      ul.setAttribute('data-wrapper-id', se.idWrapper);
 
-      li.appendChild(divImg);
-      li.appendChild(divName);
-      li.appendChild(divName);
-      ul.appendChild(li);
+      for (var j = 0, len1 = _.originalItems.length; j < len1; j++) {
+        var li = createDomLi(_.originalItems[j], j, se.idWrapper)
+        ul.appendChild(li);
+      }
+
+      container.appendChild(ul);
+      var emlContainer = container.cloneNode(true);
+
+      se.dom.parentNode.insertBefore(emlContainer, se.dom.nextSibling);
+      _.superElements[i].suggestLayer = emlContainer;
+      se.dom.setAttribute('data-list-id', se.idListItem);
+      se.dom.setAttribute('data-wrapper-id', se.idWrapper);
+
+      if (!se.dom.getAttribute('id')) {
+        se.dom.setAttribute(guid());
+      }
     }
-
-    container.appendChild(ul);
-
-    for (var i = 0, len = _.elements.length; i < len; i++) {
-      _.elements[i].parentNode.insertBefore(container, _.elements[i].nextSibling);
-    }
-
-    _.suggestLayer = container;
 
   };
 
-  $.fn.render = function () {
+  $.fn.render = function (wrapperId) {
     var _ = this;
+    console.log(wrapperId);
+
+    console.log(_.superElementsObj[wrapperId].items);
+
+
     // Virtual dom
 
     return _;
@@ -221,20 +305,21 @@
     return _;
   };
 
-  $.fn.showSuggestLayer = function () {
-    var arr = this.suggestLayer.className.split(" ");
+  $.fn.showSuggestLayer = function (wrapperId) {
+    var _ = this;
+    var arr = _.superElementsObj[wrapperId].suggestLayer.className.split(" ");
     if (arr.indexOf('active') < 0) {
-      this.suggestLayer.className += ' active';
+      _.superElementsObj[wrapperId].suggestLayer.className += ' active';
     }
   };
 
-  $.fn.hideSuggestLayer = function () {
-    this.suggestLayer.className = this.suggestLayer.className.replace(/\active\b/g, "");
-    this.suggestLayer.className = this.suggestLayer.className.trim();
+  $.fn.hideSuggestLayer = function (wrapperId) {
+    var _ = this;
+    _.superElementsObj[wrapperId].suggestLayer.className = _.superElementsObj[wrapperId].suggestLayer.className.replace(/\active\b/g, "");
+    _.superElementsObj[wrapperId].suggestLayer.className = _.superElementsObj[wrapperId].suggestLayer.className.trim();
   };
 
 })(odin);
 
 
 var aaa = odin('.i_search').ac({fap: 1}, TABLE_DATA);
-console.log(aaa);
